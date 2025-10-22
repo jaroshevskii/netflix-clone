67ef23c56ecc1e5e9c0e3bbb260bd02c9e08209f
Sasha Jaroshevskii 🦄 <72662383+jaroshvekii@users.noreply.github.com>
2025-10-22 21:27:43 +0200

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Sasha Jaroshevskii on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetflixCloneTests/NetflixCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneTests.swift
+//  NetflixCloneTests
+//
+//  Created by Sasha Jaroshevskii on 10/22/25.
+//
+
+import Testing
+@testable import NetflixClone
+
+struct NetflixCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
